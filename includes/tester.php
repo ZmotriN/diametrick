4e7322828561ff931a6550b57cf1077da40a1287
Maxime Larrivée-Roy <zmotrin@gmail.com>
2023-09-02 14:18:05 -0400

    Modal Loading

--- includes/tester.php
+++ includes/tester.php
@@ -5,7 +5,26 @@
 ?>
 
 
+<div class="modal-object modaltest" id="modal-test">asdf</div>
+
+
 <script>
 
+// const modalTest = new Modal('#modal-test');
+
+// setTimeout(() => {
+//     modalTest.show();    
+// }, 1000);
+
+
+const loading = new ModalLoading();
+
+setTimeout(() => {
+    loading.show();    
+}, 1000);
+
+setTimeout(() => {
+    loading.hide();    
+}, 4000);
     
 </script>
--- jscripts/modal.js
+++ jscripts/modal.js
@@ -1,3 +1,33 @@
 class Modal {
     
+    obj = null;
+    cont = null;
+
+    constructor(obj) {
+        this.obj = typeof obj == 'string' ? query(obj) : obj;
+        this.cont = document.createElement('div');
+        this.cont.className = 'modal';
+        this.cont.append(this.obj);
+        document.body.append(this.cont);
+    }
+
+    show() {
+        this.cont.classList.add('show');
+    }
+
+    hide() {
+        this.cont.classList.remove('show');
+    }
+
+}
+
+
+class ModalLoading extends Modal {
+
+    constructor() {
+        const loading = document.createElement('div');
+        loading.className = 'modal-object modal-loading';
+        super(loading);
+    }
+
 }
